86502d68969e300acb017689c1b07107a68f8199
Thi Dieu <huou.ho22@student.passerellesnumeriques.org>
2021-12-25 01:31:33 +0700

    Part3

--- app.js
+++ app.js
@@ -10,6 +10,42 @@ var coolRouter = require('./routes/users');
 
 var app = express();
 
+const { MongoClient } = require('mongodb');
+
+async function main() {
+   
+    const uri = "mongodb://localhost:27017";
+
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+/**
+ * Print the names of all available databases
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
+ */
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
